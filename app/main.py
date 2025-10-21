523004ba2f648a74a328eeec145e09adcbcc7717
Filipe-Pontes <filipe_cavalcante20@hotmail.com>
2025-10-21 09:44:32 -0300

    alteração da porta

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "tasks")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "321976548")
-DB_PORT = os.getenv("DB_PORT", "5433")
+DB_PORT = os.getenv("DB_PORT", "5432")
 
 class Task(BaseModel):
     task: str
